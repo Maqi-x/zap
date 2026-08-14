98a7bbeed7c8aafe8888521415a005627706a3c5
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-08-14 15:01:52 +0000

    Deploying to gh-pages from @ Maqi-x/zap@d45cc8d857223c0afe7c5af02df8885e73331c30 🚀

---
+++ d4/d2c/structzap__arc__metadata__t.js
@@ -0,0 +1,4 @@
+var structzap__arc__metadata__t =
+[
+    [ "trace_fn", "d4/d2c/structzap__arc__metadata__t.html#ad12568dc3e285e634ff22ec39611a4d2", null ]
+];
---
+++ navtreedata.js
@@ -0,0 +1,108 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "ZAP", "index.html", [
+    [ "Changelog", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html", [
+      [ "[0.1.0] - 2026-07-24", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md5", [
+        [ "Added", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md6", null ],
+        [ "Changed", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md7", null ]
+      ] ],
+      [ "[0.0.1] - 2026-03-16", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md8", [
+        [ "Added", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md9", null ],
+        [ "Changed", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md10", null ],
+        [ "Removed", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md11", null ]
+      ] ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/dd5/classsema_1_1BoundFailStatement.html#ace0e948bc80ac8b94045e64950c14942",
+"d1/d76/structzir_1_1EnumType_1_1Variant.html",
+"d2/d2e/unary__expr_8hpp_source.html",
+"d2/dba/classzap_1_1lsp_1_1JsonObject.html#adb6a6687a934d46790c9bfeae27023e0",
+"d3/d31/classzir_1_1WeakAliveInst.html#ad1f9d2ee1f29515fbe39c1acc9a1422c",
+"d3/df8/structzap_1_1lsp_1_1SemanticSnapshot.html#ab4098d57d6f01f7ac2ac8086a87fde6c",
+"d4/d8a/classzir_1_1MoveInst.html#ad80d4c9cea4348d24ae98e2ffe5fe50a",
+"d5/d23/namespacezap_1_1lsp.html#ae016f67d70061ab5aef04f5e6d4c5c6d",
+"d6/d7b/classClassDecl.html#a569fdd188f0f44dc2cd5fb5841b7a5f3",
+"d7/d50/namespacesema_1_1anonymous__namespace_02conversion_8cpp_03.html#a138bd86df29293735fd2de540217e68c",
+"d8/d58/flags_8inc.html",
+"d9/d00/classzap_1_1driver.html#a0958bb807d28a48357f784f362b51735",
+"d9/d68/classzir_1_1Module.html#a6afa2d24339b678b56f5a08adb97288c",
+"da/d53/classBinExpr.html#afcee26d77cbdd25b13b6c473aefc5e9f",
+"da/de9/classcodegen_1_1LLVMCodeGen.html#a7e67d1645fad05514fdeb2b1161e062a",
+"db/db1/classsema_1_1FunctionSymbol.html#a6f4d1e070d40200be3504bc522152863",
+"dc/d13/structVisitor.html#a38542ab4ca9cca89e729938ad3070268",
+"dc/d2c/classsema_1_1Binder.html#aada8cec31b7690f0d9eddc5b7f7280e5",
+"dc/ddd/parser_8cpp_source.html",
+"dd/db4/process_8hpp_source.html",
+"de/d73/classzap_1_1Parser.html#a9309005ceef5d5d2a9be5c7755ed1d99",
+"df/d35/namespacezir.html#a8aff9f37489c6b980b6ff3787f3a6959a61cf8510205077b6f5491d38cd44c0f7",
+"df/ddd/binder__module__predeclare_8cpp.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
+var LISTOFALLMEMBERS = 'List of all members';
